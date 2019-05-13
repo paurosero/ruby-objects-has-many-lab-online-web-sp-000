8c517ecb6aa428adce436834d1647ccd91dcaa06
Paul Rosero <paul_rosero@yahoo.com>
2019-05-13 22:39:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,19 @@
 class Song
-  attr_accessor :artist, :name
-   
+  attr_accessor :name, :artist
+  
+  @song = []
+  
   def initialize(name)
      @name = name
+     @song << self
+  end
   
+  def self.all
+    @song
+  end
+
+  def artist_name
+    artist.name if artist
   end
   
 end
